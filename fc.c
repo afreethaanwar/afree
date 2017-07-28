840b308c524b1adb8b3446aa0e1482686e82c2f6
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-07-28 23:17:18 +0530

    Create fc.c

---
+++ fc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int f=0,s=1,t,i,n;
+printf("enter the no of terms");
+scanf("%d",&n);
+printf("%d %d"first,second);
+for(i=2;i<n;i++)
+{
+t=f+s;
+printf("%d",t);
+f=s;
+s=t;
+}return 0;
+)
