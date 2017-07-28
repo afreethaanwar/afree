1a8ce961ecc8631f3084ac6a14e7cc767736645b
afreethaanwar <30343460+afreethaanwar@users.noreply.github.com>
2017-07-28 23:32:52 +0530

    Update fc.c

--- fc.c
+++ fc.c
@@ -4,7 +4,7 @@ int main()
 int f=0,s=1,t,i,n;
 printf("enter the no of terms");
 scanf("%d",&n);
-printf("%d %d"first,second);
+printf("%d%d",f,s);
 for(i=2;i<n;i++)
 {
 t=f+s;
@@ -12,4 +12,4 @@ printf("%d",t);
 f=s;
 s=t;
 }return 0;
-)
+}
